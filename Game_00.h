ef8fc1ea1987075eaa230dbc10a693bb3d9b7a25
VectorVegA <77268894+VectorVegA@users.noreply.github.com>
2022-06-27 17:43:06 +0300

    +++

--- Game_00.h
+++ Game_00.h
@@ -3,8 +3,51 @@
 #include <LIBCHAR.h>
 
 
+void printsimbol(void)
+{
+    char j = 0;
+
+    while(1)
+    {
+
+        int i = 80;
+
+        printf("%d\n",j);
+        while(i>0)
+        {
+            putchar(j);
+            i--;
+        }
+        LIB_CHAR_WAIT_ENTER();
+        j++;
+
+    }
+}
+
 void game_00()
 {
+
+    char * var_str = NULL;
+    char var_term_len = 80;
+
+    var_str = LIB_CHAR_Public_Get_String();
+
+
+    char var_str_len = LIB_CHAR_Public_Str_Lenght(var_str);
+
+    char abc = var_term_len - var_str_len;
+    if(abc < 0) LIB_CHAR_Public_Put_String((char*)"ERROR !!! ::: var_str_len > 80");
+
+    for(int i = 0; i< abc; i ++) putchar(' ');
+    LIB_CHAR_Public_Put_String(var_str);
+
+
+
+
+    return;
+
+
+
 
 
    LIB_CHAR_Public_Put_String((char*)"\n\t\t\t=================================\n");
--- LIBCHAR.h
+++ LIBCHAR.h
@@ -4,6 +4,24 @@
 #include <stdlib.h>
 #include <conio.h>
 
+#define BOOL char
+#define FALSE 0
+#define TRUE 1
+#define EOT "EOT"
+
+
+char* LIB_CHAR_Free_Staring(char* var_str)
+{
+    printf("\LIB_CHAR_Free_Staringn");
+    if(var_str == NULL) return var_str;
+
+    var_str = (char*)realloc(var_str,0);
+
+    var_str = NULL;
+
+    return var_str;
+}
+
 void fun_private_putchar_digit(char var_c )
 {
     if( var_c ==0) putchar('0');
@@ -82,48 +100,73 @@ void LIB_CHAR_Public_Putchar_Number(char var_c )
     /////////////
     return;
 }
-
-int LIB_CHAR_Public_Str_Lenght(char *var_str)/// функция под названием LIB_CHAR_Public_Str_Lenght жрет var_str и то что прилетает в облать памяти char* и какает интом
+int LIB_CHAR_Public_Str_Lenght(char *var_str)
 {
-    if(var_str == NULL) return -1; /// если переменная var_str равна  переменной NULL возвращаем -1
-    int i = 0;/// создаем областьпамяти инт для переменной i которая равна 0
-    while(1) /// создаем замкнутый цикл
+    printf("4\n");
+    if(var_str == NULL)
     {
-        if (var_str[i] == '\0') return i;/// если (буква var_str[i] из строки var_str равна концу строки то вернуть порядковый номер буквы;
-        i = i+1;/// к старому адресу буквы прнибавляем единицу чтоб получить адрес буквы в строке
+        printf("5\n");
+        return -1;
     }
-}
-char* LIB_CHAR_Public_Append_Char_To_String(char* var_str,char var_bukva)   ///5 заголовок функции LIB_CHAR_Public_Append_Char_To_String которая возвращает char* и на вход берёт char* var_str,char var_bukva
-{   ///6 если по русскти то эта функция доюавляеь букву в строку. тоесть она кушает стороку, кушает букву, и какаяет новой строкой в конце которой будет добавлена  скушанная буква
-    ///7   йцу э ----> йцуэ
-    if(var_str == NULL) ///8 если пееменная var_str равна NULL то делаем следующее
-
+    printf("6\n");
+    int i = 0;
+     printf("7\n");
+    while(1)
     {
-        var_str = (char*) malloc (2*sizeof (char)); ///9 функция oc кушац sizeof 2 раза и то что прилетает в область памяти char. понятное дело, что она не блекхол и не может только кушать, поэтому она какает var_str
-        var_str[0] = '\0';///10 могу ошибаться .если var_str ноль то переходим на следущую строку
+        printf("         %d         8\n",i);
+        printf("    ----------->    %s   %d \n",var_str,i);
+
+
+        if (var_str[i] == '\0')
+        {
+            printf("9\n");
+            return i;
+        }
+        printf("10\n");
+        printf("\t %d) \n');",i,var_str[i]);
+        i = i+1;
     }
-
-    int var_str_len =  LIB_CHAR_Public_Str_Lenght(var_str); ///  создаем область памяти  var_str_len положить результат вызова функции LIB_CHAR_Public_Str_Lenght(то что кушает функция (строка)) какает числом которое является длинной строки.
-    var_str = (char*)realloc(var_str,var_str_len + 2);/// var_str = (создаем область памяти с неизвестной длинной) расширяем область памяти (var_str,var_str_len + 2)
-
-    var_str[var_str_len] = var_bukva;  ///даем переменной var_sзначение var_bukva
-    var_str[var_str_len + 1] = '\0';   ///увеличиваем значение var_str_len на 1 с целью узнать номер строки
-    return var_str; /// возвращаем var_str
+    printf("11\n");
 }
-void LIB_CHAR_Public_Put_String(char* var_str) /// вызываем функцию LIB_CHAR_Public_Put_String которая на вход берет строку и возвращает пустоту
+char* LIB_CHAR_Public_Append_Char_To_String(char* var_str,char var_bukva)
 {
-    unsigned int i = 0;  /// задаем переменной i значение 0
-    while(1)  /// создаем замкнутый цикл
+
+printf("\n\n\tLIB_CHAR_Public_Append_Char_To_String\n");
+
+
+    if(var_str == NULL)
     {
-        if(var_str[i] == '\0') /// если строка начнается с символа окончания строки
+        var_str = (char*) malloc (2*sizeof (char));
+        var_str[0] = '\0';
+        printf("2\n");
+    }
+
+    printf("\n\n3\n");
+    int var_str_len = 50;// LIB_CHAR_Public_Str_Lenght(var_str);
+
+
+    var_str = (char*)realloc(var_str,(var_str_len + 2));
+printf("\n\n  pos\n");
+    var_str[var_str_len] = var_bukva;
+
+    var_str[var_str_len + 1] = '\0';
+
+    return var_str;
+}
+void LIB_CHAR_Public_Put_String(char* var_str)
+{
+    unsigned int i = 0;
+    while(1)
+    {
+        if(var_str[i] == '\0')
         {
             ///putchar('\n'); /// печатаем '\n'
-            break;  /// останавливаем цикл
+            break;
         }
-        putchar(var_str[i]); /// печатаем строку и ее номер
-        i = i + 1; /// последовательно увеличиваем номер строки
+        putchar(var_str[i]);
+        i = i + 1;
     }
-    return; /// ничего не возвращаем
+    return;
 }
 char* LIB_CHAR_Public_Reverse_String(char* var_str) /// вызываем функцию LIB_CHAR_Public_Reverse_String которая берет на вход строку с неизвестным количеством символов и возвращает неизвестное количество символов
 {
@@ -146,11 +189,11 @@ void LIB_CHAR_WAIT_ENTER(void)
    char c =0 ;
    while(c!=13)  c = getch();
 }
-
 void LIB_CHAR_help(void)
 {
-
     LIB_CHAR_Public_Put_String((char*)"USE:");
+
+
     LIB_CHAR_Public_Put_String((char*)"\n ---> LIB_CHAR_Public_Putchar_Bits( byte);            print number bit by bit\n");
     LIB_CHAR_Public_Put_String((char*)"\n ---> LIB_CHAR_Public_Putchar_Number( var_c );        print number \n");
     LIB_CHAR_Public_Put_String((char*)"\n ---> LIB_CHAR_Public_Str_Lenght( var_str );          return lenght of string\n");
@@ -158,13 +201,10 @@ void LIB_CHAR_help(void)
     LIB_CHAR_Public_Put_String((char*)"\n ---> LIB_CHAR_Public_Put_String( var_c );            print string \n");
     LIB_CHAR_Public_Put_String((char*)"\n ---> LIB_CHAR_Public_Reverse_String( var_c );        reverse string\n");
 
-
-
 }
-
-
 char* LIB_CHAR_Public_Get_String(void)
 {
+     printf("\n\n\t\tLIB_CHAR_Public_Get_String\n");
     char* var_str = NULL;
 
 
@@ -179,8 +219,163 @@ char* LIB_CHAR_Public_Get_String(void)
 
 }
 
-#endif // LIBCHAR_H
 
+
+BOOL LIB_CHAR_STR_CMP(char* var_str_1,char* var_str_2)
+{
+    printf("LIB_CHAR_STR_CMP");
+    if( var_str_1 == NULL) return FALSE;
+    if( var_str_2 == NULL) return FALSE;
+
+
+    int var_len_1 = LIB_CHAR_Public_Str_Lenght(var_str_2);
+    int var_len_2 = LIB_CHAR_Public_Str_Lenght(var_str_2);
+
+
+    if(var_len_1<0) return FALSE;
+    if(var_len_2<0) return FALSE;
+
+    if(var_len_2 != var_len_1) return FALSE;
+
+    int i = 0;
+    while(1)
+    {
+        if(var_str_1[i]!=var_str_2[i])return FALSE;
+        i++;
+        if(i == var_len_1) break;
+    }
+    return TRUE;
+}
+
+
+int LIB_CHAR_Public_Txt_Lenght(char **var_Text)
+{
+    if(var_Text == NULL) return -1;
+    int i = 0;
+    while(1)
+    {
+        if (LIB_CHAR_STR_CMP(var_Text[i],(char*)EOT)) return i;
+        i = i+1;
+    }
+}
+ char * LIB_CHAR_Public_Append_Sting_To_String(char * var_string_1, char* var_string_2)
+ {
+     printf("LIB_CHAR_Public_Append_Sting_To_String");
+
+     if(var_string_2 == NULL) return var_string_1;
+
+     int var_len = LIB_CHAR_Public_Str_Lenght(var_string_2);
+
+
+
+
+     int i = 0;
+     while(1)
+     {
+
+
+         var_string_1 = LIB_CHAR_Public_Append_Char_To_String(var_string_1, var_string_2[i]);
+
+
+
+         if(i==var_len) break;
+         i++;
+     }
+
+    return var_string_1;
+ }
+
+
+
+char** LIB_CHAR_Public_Append_String_To_Text(char**var_Text,char*var_str)
+{
+
+    if(var_str == NULL)
+    {
+        return var_Text;
+
+    }
+
+    int var_txt_len =  LIB_CHAR_Public_Txt_Lenght(var_Text);
+
+
+    if(var_txt_len == -1)
+    {
+        var_Text = (char**) malloc (2*sizeof (char*));
+        var_Text[0] = LIB_CHAR_Public_Append_Sting_To_String(var_Text[0], (char*)EOT);
+
+    }
+
+    var_Text = (char**)realloc(var_Text,var_txt_len + 2);
+
+    var_Text[var_txt_len] = LIB_CHAR_Public_Append_Sting_To_String(var_Text[var_txt_len],var_str);
+
+    var_Text[var_txt_len] = LIB_CHAR_Public_Append_Sting_To_String(var_Text[var_txt_len],(char*)EOT);
+
+
+    return var_Text;
+
+
+    return var_Text;
+}
+
+char** LIB_CHAR_Public_Get_Text(void)
+{
+
+    printf("1\n");
+
+    char** var_Text = NULL;
+
+    while(1)
+    {
+        char *var_str = LIB_CHAR_Public_Get_String();
+
+        var_Text = LIB_CHAR_Public_Append_String_To_Text(var_Text,var_str);
+
+
+
+
+        if(LIB_CHAR_STR_CMP(var_str,(char*)EOT))
+        {
+            var_str = LIB_CHAR_Free_Staring(var_str);
+            break;
+        }
+    }
+
+    return var_Text;
+}
+
+void LIB_CHAR_Public_Put_Text(char**var_Text)
+{
+
+    unsigned int i = 0;
+    while(1)
+    {
+        if(LIB_CHAR_STR_CMP(var_Text[i],(char*)EOT)) break;
+        LIB_CHAR_Public_Put_String(var_Text[i]);
+        i = i + 1;
+    }
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // LIBCHAR_H
 
 
 
